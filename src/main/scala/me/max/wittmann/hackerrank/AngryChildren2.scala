9f85d4ba9de52a2f48ad59ab4545bdd6a1440fbd
Maximilian Wittmann <Maximilian.Wittmann@cba.com.au>
2016-12-10 16:07:39 +1100

    Workin' on the the angry kids (part deux)

--- src/main/scala/me/max/wittmann/hackerrank/AngryChildren2.scala
+++ src/main/scala/me/max/wittmann/hackerrank/AngryChildren2.scala
@@ -7,10 +7,11 @@ object AngryChildren2 {
   }
 
   def solve(candies: List[Int], children: Int): Int = {
+    val candiesS = candies.sorted
+
     val prefixes =
-      candies
-        .sorted
-        .foldLeft(candies.take(1))((li, cur) => {
+      candiesS
+        .foldLeft(candiesS.take(1))((li, cur) => {
           (cur + li.head) :: li
         })
         .reverse
@@ -19,15 +20,36 @@ object AngryChildren2 {
 //      prefixes(i + children - 1) - prefixes(i)
 //    }).min
 
+    val seedValue =
+      (0 to children-1).toList
+        .map(i => (0 to children - 1).toList
+          .map(j => {
+            Math.abs(candiesS(i) - candiesS(j))
+          }).sum).sum
+
+    println(s"Prefixes: $prefixes\nSeed value: $seedValue")
+
+    val diffs =
+      (children to candiesS.length - 1).foldLeft(List(seedValue))((r, i) => {
+        val i_minus_1: Int = r.head
+
+        val i_diff: Int = candiesS(i) * children - (prefixes(i) - prefixes(i - children)) //i-children-1?
+
+        val i_minus_k_diff: Int = candiesS(i - children) * children - (prefixes(i) - prefixes(i - children)) //i-children-1?
+
+        (i_minus_1 + i_diff - i_minus_k_diff) :: r
+      })
+
+    println(s"Diffs: $diffs")
+
+    val minDiff = diffs.min / 2
+
+    minDiff
 
 
-
-
-
-    (children + 1 until candies.length - 1).map(i => {
-
-    })
-
+//    (children + 1 until candies.length - 1).map(i => {
+//
+//    })
   }
 
 }
